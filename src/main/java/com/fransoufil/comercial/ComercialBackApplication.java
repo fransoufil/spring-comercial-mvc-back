6436bf15d596ac039c8ac2e99fcb98be5b351055
fransoufil <fran.sf@uol.com.br>
2021-05-30 15:16:28 -0300

    Projeto criado

---
+++ src/main/java/com/fransoufil/comercial/ComercialBackApplication.java
@@ -0,0 +1,13 @@
+package com.fransoufil.comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercialBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercialBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fransoufil/comercial/ComercialBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fransoufil.comercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercialBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
