7b9b7997f56be1228e78183950b9a6adcac7d016
fransoufil <fran.sf@uol.com.br>
2021-05-30 15:29:58 -0300

    Testando a primeira classe de domínio: Categoria

--- src/main/java/com/fransoufil/comercial/resources/CategoriaResource.java
+++ src/main/java/com/fransoufil/comercial/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.fransoufil.comercial.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fransoufil.comercial.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> findAll() {
+		
+	  Categoria cat1 = new Categoria(1, "Informática");
+      Categoria cat2 = new Categoria(2, "Escritório");
+
+      List<Categoria> lista = new ArrayList<>();
+      
+      lista.add(cat1);
+      lista.add(cat2);
+
+      return lista;
+		
 	}
 
 }
