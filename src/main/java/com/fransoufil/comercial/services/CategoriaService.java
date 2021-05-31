3fdf2696646092a9310f1cd4489804c97914cf39
fransoufil <fran.sf@uol.com.br>
2021-05-31 20:16:55 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/fransoufil/comercial/services/CategoriaService.java
+++ src/main/java/com/fransoufil/comercial/services/CategoriaService.java
@@ -1,10 +1,15 @@
 package com.fransoufil.comercial.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fransoufil.comercial.domain.Categoria;
 import com.fransoufil.comercial.repositories.CategoriaRepository;
+import com.fransoufil.comercial.services.exceptions.ObjectNotFoundException;
+
+import net.bytebuddy.implementation.bytecode.Throw;
 
 @Service
 public class CategoriaService {
@@ -13,8 +18,9 @@ public class CategoriaService {
 	private CategoriaRepository categoriaRepository;
 	
 	public Categoria find(Integer id) {
-		Categoria obj = categoriaRepository.findById(id).get();
-		return obj;
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
